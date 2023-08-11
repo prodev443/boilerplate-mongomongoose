13acee8d8a78ef97e11f1523a7271e3f2ea83f85
Samuel Alderete Rayón <92610746+sammymalware@users.noreply.github.com>
2023-08-11 17:29:30 -0600

    Usa model.findOne() para devolver un único documento coincidente de tu base de datos

--- myApp.js
+++ myApp.js
@@ -39,7 +39,15 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne(
+    {
+      favoriteFoods: food,
+    },
+    (err, data) => {
+      if (err) return console.log(err);
+      done(null, data);
+    }
+  );
 };
 
 const findPersonById = (personId, done) => {
