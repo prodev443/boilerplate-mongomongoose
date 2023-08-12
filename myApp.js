b912d6c8f5a51e04b0e66fdf51fb246fb8b79aed
Samuel Alderete Rayón <92610746+sammymalware@users.noreply.github.com>
2023-08-11 18:05:12 -0600

    feat: Nuevas actualizaciones en un documento
    Realiza nuevas actualizaciones en un documento usando
    model.findOneAndUpdate()

--- myApp.js
+++ myApp.js
@@ -86,7 +86,15 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
