3cf385864cc6747aea572915627ecfa1c9501577
Samuel Alderete Rayón <92610746+sammymalware@users.noreply.github.com>
2023-08-11 18:10:33 -0600

    feat: Elimina un documento por Id
    Elimina un documento usando el método model.findByIdAndRemove()

--- myApp.js
+++ myApp.js
@@ -98,7 +98,13 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(
+    personId,
+    (err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    }
+  );
 };
 
 const removeManyPeople = (done) => {
